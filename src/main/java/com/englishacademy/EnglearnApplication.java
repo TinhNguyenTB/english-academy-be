ae9555e09407ea46491decb44e717cb2bd81511f
TinhNguyenTB <tinh271103@gmail.com>
2025-06-22 10:22:32 +0700

    init project

---
+++ src/main/java/com/englishacademy/EnglearnApplication.java
@@ -0,0 +1,13 @@
+package com.englishacademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnglearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnglearnApplication.class, args);
+	}
+
+}
