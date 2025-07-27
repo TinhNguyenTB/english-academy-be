0e265df1f04d0ff3b4090ce1eabeb3b735c8a1fb
hoanglong2534 <phamxuanhoanglong@gmail.com>
2025-07-27 18:46:44 +0700

    be - update UserAnswerController

--- src/main/java/com/englishacademy/controller/UserAnswerController.java
+++ src/main/java/com/englishacademy/controller/UserAnswerController.java
@@ -4,7 +4,6 @@ import com.englishacademy.dto.request.UserAnswerRequestDTO;
 import com.englishacademy.entity.UserAnswer;
 import com.englishacademy.service.UserAnswerService;
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
