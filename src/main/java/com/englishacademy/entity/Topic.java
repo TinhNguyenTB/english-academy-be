20f41e2c902a3dc55a4b580cbb9fe721e8343840
hoanglong2534 <phamxuanhoanglong@gmail.com>
2025-07-28 23:33:26 +0700

    be - update entities

---
+++ src/main/java/com/englishacademy/entity/Topic.java
@@ -0,0 +1,41 @@
+package com.englishacademy.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "topics")
+public class Topic {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false, unique = true)
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name= "order_index", nullable = false)
+    private int orderIndex;
+
+    @Column(name = "price", nullable = false)
+    private double price;
+
+    @Column(name = "is_free", nullable = false)
+    private boolean isFree;
+
+    @OneToMany(mappedBy = "topic", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Lesson> lessons;
+
+}
