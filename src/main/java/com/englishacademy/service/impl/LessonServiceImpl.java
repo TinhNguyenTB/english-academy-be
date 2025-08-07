ffa5e7f96d9aa4e3365592ae2167c8d2371bb02a
hoanglong2534 <phamxuanhoanglong@gmail.com>
2025-08-07 18:43:55 +0700

    cache Lesson

--- src/main/java/com/englishacademy/service/impl/LessonServiceImpl.java
+++ src/main/java/com/englishacademy/service/impl/LessonServiceImpl.java
@@ -1,6 +1,7 @@
 package com.englishacademy.service.impl;
 
 import com.englishacademy.dto.request.LessonRequestDTO;
+import com.englishacademy.dto.response.LessonResponeDTO;
 import com.englishacademy.entity.Lesson;
 import com.englishacademy.entity.Topic;
 import com.englishacademy.mapper.LessonMapper;
@@ -9,10 +10,13 @@ import com.englishacademy.repository.TopicRepository;
 import com.englishacademy.service.LessonService;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import java.util.List;
+
 @RequiredArgsConstructor
 @Service
 @FieldDefaults(makeFinal = true)
@@ -22,59 +26,68 @@ public class LessonServiceImpl implements LessonService {
     private LessonMapper lessonMapper;
     private TopicRepository topicRepository;
 
+    @Cacheable(value = "LESSON_CACHE", key = "#id")
     @Override
-    public Lesson getLessonById(Long id) {
-        return lessonRepository.findById(id).orElse(null);
+    public LessonResponeDTO getLessonById(Long id) {
+        Lesson lesson = lessonRepository.findById(id).orElse(null);
+        return lessonMapper.toResponeDTO(lesson);
     }
 
     @Override
-    public Page<Lesson> getAllLessons(Pageable pageable) {
-        return lessonRepository.findAll(pageable);
+    public Page<LessonResponeDTO> getAllLessons(Pageable pageable) {
+        return lessonRepository.findAll(pageable).map(lessonMapper::toResponeDTO);
     }
 
-
+    @CachePut(value = "LESSON_CACHE", key="#result.id")
     @Override
-    public void createLesson(LessonRequestDTO requestDTO) {
+    public LessonResponeDTO createLesson(LessonRequestDTO requestDTO) {
         Lesson lesson = lessonMapper.toEntity(requestDTO);
         Topic topic = topicRepository.findById(requestDTO.getTopicId())
                 .orElseThrow(()-> new RuntimeException("Topic not found"));
         lesson.setTopic(topic);
         lessonRepository.save(lesson);
+        return lessonMapper.toResponeDTO(lesson);
     }
 
+    @CacheEvict(value = "LESSON_CACHE", key="#id")
     @Override
     public void deleteLesson(Long id) {
         lessonRepository.deleteById(id);
     }
 
+    @CachePut(value = "LESSON_CACHE", key = "#result.id")
     @Override
-    public void updateLesson(Long id, LessonRequestDTO lesson) {
+    public LessonResponeDTO updateLesson(Long id, LessonRequestDTO lesson) {
         Lesson oldLesson = lessonRepository.findById(id).get();
         lessonMapper.updateEntityFromDto(lesson, oldLesson);
         lessonRepository.save(oldLesson);
+        return lessonMapper.toResponeDTO(oldLesson);
     }
 
     @Override
-    public Page<Lesson> findByName(String name, Pageable pageable) {
+    public Page<LessonResponeDTO> findByName(String name, Pageable pageable) {
+        Page<Lesson> lessons;
        if(!name.isBlank()){
-           return lessonRepository.findByNameContainsIgnoreCase(name, pageable);
+           lessons = lessonRepository.findByNameContainsIgnoreCase(name, pageable);
+       }else{
+           lessons = lessonRepository.findAll(pageable);
        }
-         return lessonRepository.findAll(pageable);
+       return lessons.map(lessonMapper::toResponeDTO);
     }
 
     @Override
-    public Page<Lesson> findByTopicId(Long topicId, Pageable pageable) {
-        return lessonRepository.findByTopicId(topicId, pageable);
+    public Page<LessonResponeDTO> findByTopicId(Long topicId, Pageable pageable) {
+        return lessonRepository.findByTopicId(topicId, pageable).map(lessonMapper::toResponeDTO);
     }
 
     @Override
-    public Page<Lesson> findByOrderIndex(int orderIndex, Pageable pageable) {
-        return lessonRepository.findByOrderIndex(orderIndex, pageable);
+    public Page<LessonResponeDTO> findByOrderIndex(int orderIndex, Pageable pageable) {
+        return lessonRepository.findByOrderIndex(orderIndex, pageable).map(lessonMapper::toResponeDTO);
     }
 
     @Override
-    public Page<Lesson> findByTotalQuestion(int totalQuestion, Pageable pageable) {
-        return lessonRepository.findByTotalQuestion(totalQuestion, pageable);
+    public Page<LessonResponeDTO> findByTotalQuestion(int totalQuestion, Pageable pageable) {
+        return lessonRepository.findByTotalQuestion(totalQuestion, pageable).map(lessonMapper::toResponeDTO);
     }
 
 }
