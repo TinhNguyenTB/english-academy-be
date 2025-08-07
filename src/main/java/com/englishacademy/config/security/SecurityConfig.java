e94caac3db5ac079718188f1d1b98dd5521cf52f
hoanglong2534 <phamxuanhoanglong@gmail.com>
2025-08-07 23:09:22 +0700

    add Swagger

--- src/main/java/com/englishacademy/config/security/SecurityConfig.java
+++ src/main/java/com/englishacademy/config/security/SecurityConfig.java
@@ -36,7 +36,10 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.authorizeHttpRequests(request ->
-                request.requestMatchers(HttpMethod.POST, PUBLIC_ENDPOINTS).permitAll()
+                request.requestMatchers( "/v3/api-docs/**",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html").permitAll()
+                        .requestMatchers(HttpMethod.POST, PUBLIC_ENDPOINTS).permitAll()
                         .requestMatchers("/api/v1/**").hasRole("ADMIN")
                         .anyRequest()
                         .authenticated()
