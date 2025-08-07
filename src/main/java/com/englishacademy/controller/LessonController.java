ebcf46661c642aad1f351f4a64ceff06cd5b56e8
hoanglong2534 <phamxuanhoanglong@gmail.com>
2025-08-07 18:43:55 +0700

    cache Lesson

--- src/main/java/com/englishacademy/controller/LessonController.java
+++ src/main/java/com/englishacademy/controller/LessonController.java
@@ -2,6 +2,7 @@ package com.englishacademy.controller;
 
 import com.englishacademy.config.locale.Translator;
 import com.englishacademy.dto.request.LessonRequestDTO;
+import com.englishacademy.dto.response.LessonResponeDTO;
 import com.englishacademy.dto.response.ResponseData;
 import com.englishacademy.dto.response.UserResponse;
 import com.englishacademy.entity.Lesson;
@@ -28,9 +29,9 @@ public class LessonController {
         this.lessonService = lessonService;
     }
     @GetMapping("/get")
-    public ResponseData<Page<Lesson>> getAllLessons(Pageable pageable) {
-        Page<Lesson> lessons = lessonService.getAllLessons(pageable);
-        return ResponseData.<Page<Lesson>>builder()
+    public ResponseData<Page<LessonResponeDTO>> getAllLessons(Pageable pageable) {
+        Page<LessonResponeDTO> lessons = lessonService.getAllLessons(pageable);
+        return ResponseData.<Page<LessonResponeDTO>>builder()
                 .message(Translator.toLocale("lesson.get.all.success"))
                 .data(lessons)
                 .code(HttpStatus.OK.value())
@@ -38,9 +39,9 @@ public class LessonController {
     }
 
     @GetMapping("/{id}")
-    public ResponseData<Lesson> getLessonById(@PathVariable Long id) {
-        Lesson lesson = lessonService.getLessonById(id);
-        return ResponseData.<Lesson>builder()
+    public ResponseData<LessonResponeDTO> getLessonById(@PathVariable Long id) {
+        LessonResponeDTO lesson = lessonService.getLessonById(id);
+        return ResponseData.<LessonResponeDTO>builder()
                 .message(Translator.toLocale("lesson.get.by.id.success"))
                 .data(lesson)
                 .code(HttpStatus.OK.value())
@@ -48,11 +49,12 @@ public class LessonController {
     }
 
     @PostMapping("/create")
-    public ResponseData<Void> createLesson(@Valid @RequestBody LessonRequestDTO lessonRequestDTO) {
-        lessonService.createLesson(lessonRequestDTO);
-        return ResponseData.<Void>builder()
+    public ResponseData<LessonResponeDTO> createLesson(@Valid @RequestBody LessonRequestDTO lessonRequestDTO) {
+        LessonResponeDTO lessonResponeDTO = lessonService.createLesson(lessonRequestDTO);
+        return ResponseData.<LessonResponeDTO>builder()
                 .message(Translator.toLocale(Translator.toLocale("lesson.create.success")))
                 .code(HttpStatus.CREATED.value())
+                .data(lessonResponeDTO)
                 .build();
     }
 
@@ -66,18 +68,19 @@ public class LessonController {
     }
 
     @PutMapping("/{id}")
-    public ResponseData<Void> updateLesson(@PathVariable Long id, @Valid  @RequestBody  LessonRequestDTO lessonRequestDTO) {
-        lessonService.updateLesson(id, lessonRequestDTO);
-        return ResponseData.<Void>builder()
+    public ResponseData<LessonResponeDTO> updateLesson(@PathVariable Long id, @Valid  @RequestBody  LessonRequestDTO lessonRequestDTO) {
+        LessonResponeDTO lessonResponeDTO = lessonService.updateLesson(id, lessonRequestDTO);
+        return ResponseData.<LessonResponeDTO>builder()
                 .message(Translator.toLocale("lesson.update.success"))
                 .code(HttpStatus.OK.value())
+                .data(lessonResponeDTO)
                 .build();
     }
 
     @GetMapping("/find-name")
-    public ResponseData<Page<Lesson>> findByName(@RequestParam @NotBlank String name, Pageable pageable) {
-        Page<Lesson> lessons = lessonService.findByName(name, pageable);
-        return ResponseData.<Page<Lesson>>builder()
+    public ResponseData<Page<LessonResponeDTO>> findByName(@RequestParam @NotBlank String name, Pageable pageable) {
+        Page<LessonResponeDTO> lessons = lessonService.findByName(name, pageable);
+        return ResponseData.<Page<LessonResponeDTO>>builder()
                 .message(Translator.toLocale("lesson.find.by.name.success"))
                 .data(lessons)
                 .code(HttpStatus.OK.value())
@@ -85,9 +88,9 @@ public class LessonController {
     }
 
     @GetMapping("/find-topic")
-    public ResponseData<Page<Lesson>> findByTopicId(@RequestParam @NotNull Long topicId, Pageable pageable) {
-        Page<Lesson> lessons = lessonService.findByTopicId(topicId, pageable);
-        return ResponseData.<Page<Lesson>>builder()
+    public ResponseData<Page<LessonResponeDTO>> findByTopicId(@RequestParam @NotNull Long topicId, Pageable pageable) {
+        Page<LessonResponeDTO> lessons = lessonService.findByTopicId(topicId, pageable);
+        return ResponseData.<Page<LessonResponeDTO>>builder()
                 .message(Translator.toLocale("lesson.find.by.topicId.success"))
                 .data(lessons)
                 .code(HttpStatus.OK.value())
@@ -95,9 +98,9 @@ public class LessonController {
     }
 
     @GetMapping("/find-order")
-    public ResponseData<Page<Lesson>> findByOrderIndex(@RequestParam @Min(1) int orderIndex, Pageable pageable) {
-        Page<Lesson> lessons = lessonService.findByOrderIndex(orderIndex, pageable);
-        return ResponseData.<Page<Lesson>>builder()
+    public ResponseData<Page<LessonResponeDTO>> findByOrderIndex(@RequestParam @Min(1) int orderIndex, Pageable pageable) {
+        Page<LessonResponeDTO> lessons = lessonService.findByOrderIndex(orderIndex, pageable);
+        return ResponseData.<Page<LessonResponeDTO>>builder()
                 .message(Translator.toLocale("lesson.find.by.orderIndex.success"))
                 .data(lessons)
                 .code(HttpStatus.OK.value())
@@ -105,9 +108,9 @@ public class LessonController {
     }
 
     @GetMapping("/find-total")
-    public ResponseData<Page<Lesson>> findByTotalQuestion(@RequestParam @Min(0) int totalQuestion, Pageable pageable) {
-        Page<Lesson> lessons = lessonService.findByTotalQuestion(totalQuestion, pageable);
-        return ResponseData.<Page<Lesson>>builder()
+    public ResponseData<Page<LessonResponeDTO>> findByTotalQuestion(@RequestParam @Min(0) int totalQuestion, Pageable pageable) {
+        Page<LessonResponeDTO> lessons = lessonService.findByTotalQuestion(totalQuestion, pageable);
+        return ResponseData.<Page<LessonResponeDTO>>builder()
                 .message(Translator.toLocale("lesson.find.by.totalQuestion.success"))
                 .data(lessons)
                 .code(HttpStatus.OK.value())
--- src/main/java/com/englishacademy/service/LessonService.java
+++ src/main/java/com/englishacademy/service/LessonService.java
@@ -1,29 +1,30 @@
 package com.englishacademy.service;
 
 import com.englishacademy.dto.request.LessonRequestDTO;
+import com.englishacademy.dto.response.LessonResponeDTO;
 import com.englishacademy.entity.Lesson;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface LessonService {
 
-    Lesson getLessonById(Long id);
+    LessonResponeDTO getLessonById(Long id);
 
-    Page<Lesson> getAllLessons(Pageable pageable);
+    Page<LessonResponeDTO> getAllLessons(Pageable pageable);
 
-    void createLesson(LessonRequestDTO lesson);
+    LessonResponeDTO createLesson(LessonRequestDTO lesson);
 
     void deleteLesson(Long id);
 
-    void updateLesson(Long id, LessonRequestDTO lesson);
+    LessonResponeDTO updateLesson(Long id, LessonRequestDTO lesson);
 
-    Page<Lesson> findByName(String name, Pageable pageable);
+    Page<LessonResponeDTO> findByName(String name, Pageable pageable);
 
-    Page<Lesson> findByTopicId(Long topicId, Pageable pageable);
+    Page<LessonResponeDTO> findByTopicId(Long topicId, Pageable pageable);
 
-    Page<Lesson> findByOrderIndex(int orderIndex, Pageable pageable);
+    Page<LessonResponeDTO> findByOrderIndex(int orderIndex, Pageable pageable);
 
-    Page<Lesson> findByTotalQuestion(int totalQuestion, Pageable pageable);
+    Page<LessonResponeDTO> findByTotalQuestion(int totalQuestion, Pageable pageable);
 
 
 }
