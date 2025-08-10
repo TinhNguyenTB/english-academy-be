39439ea2ecc5aa3585c40befe6ce8542c16f8a04
hoanglong2534 <phamxuanhoanglong@gmail.com>
2025-08-10 15:41:17 +0700

    Schedule retry failed email

---
+++ src/main/java/com/englishacademy/entity/EmailEntity.java
@@ -0,0 +1,49 @@
+package com.englishacademy.entity;
+
+
+import com.englishacademy.enums.EmailStatusEnum;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Table(name = "emails")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+public class EmailEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="email_from")
+    private String from;
+
+    @Column(name="email_to")
+    private String to;
+
+    @Column(name="subject")
+    private String subject;
+
+    @Column(name="content", columnDefinition = "TEXT")
+    private String content;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name="status")
+    private EmailStatusEnum status;
+
+    @Column(name="retryNum")
+    private int retryNum;
+
+    @Column(name="createAt")
+    private LocalDateTime createAt;
+
+    @Column(name="lastTryAt")
+    private LocalDateTime lastTryAt;
+}
