39e8d5588ed7ad48751417f695d6feedec1f356a
hoanglong2534 <phamxuanhoanglong@gmail.com>
2025-08-17 01:10:48 +0700

    update kafka retry and dlt

---
+++ src/main/java/com/englishacademy/entity/FailedEmail.java
@@ -0,0 +1,40 @@
+package com.englishacademy.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "failed_mail")
+public class FailedEmail {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="email_to")
+    private String to;
+
+    @Column(name="subject")
+    private String subject;
+
+    @Column(name="body", columnDefinition = "TEXT")
+    private String body;
+
+    @Column(name="retry_number")
+    private int retryNumber;
+
+    @Column(name="create_at")
+    private LocalDateTime createdAt;
+
+    @Column(name="last_retry_time")
+    private LocalDateTime lastRetryTime;
+}
