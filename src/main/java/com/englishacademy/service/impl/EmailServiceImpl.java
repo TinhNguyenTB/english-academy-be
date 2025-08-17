ab8586de91ae17af79c61b88ae665dc2873c1eae
hoanglong2534 <phamxuanhoanglong@gmail.com>
2025-08-17 20:07:26 +0700

    add check isEmail before send

--- src/main/java/com/englishacademy/service/impl/EmailServiceImpl.java
+++ src/main/java/com/englishacademy/service/impl/EmailServiceImpl.java
@@ -36,8 +36,30 @@ public class EmailServiceImpl implements EmailService {
     @Value("${SENDGRID_FROM_EMAIL}")
     private String fromEmail;
 
+    private boolean isValidEmail(String email) {
+        if (email == null) return false;
+        return email.matches("^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$");
+    }
+    private void saveFailedEmail(String to, String subject, String content) {
+        EmailMessageDTO emailMessageDTO = new EmailMessageDTO();
+        emailMessageDTO.setTo(to);
+        emailMessageDTO.setSubject(subject);
+        emailMessageDTO.setBody(content);
+        emailMessageDTO.setRetryNumber(0);
+        emailMessageDTO.setCreateAt(LocalDateTime.now());
+        emailMessageDTO.setLastRetryTime(LocalDateTime.now());
+
+        failedEmailRepository.save(emailMapper.toEntity(emailMessageDTO));
+        log.info("Saved failed email to DB: {} (reason: {})", to);
+    }
     @Override
     public void sendEmail(String to, String subject, String content) {
+        if (!isValidEmail(to)) {
+            log.warn("Invalid email format: {}", to);
+            saveFailedEmail(to, subject, content);
+            return;
+        }
+
         SendGrid sendGrid = new SendGrid(sendGridKey);
 
         Email from = new Email(fromEmail);
